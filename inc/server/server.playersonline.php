be2a2d5121d144a174f32e40299fa82a34d0d278
paintballrefjosh <paintballrefjosh@gmail.com>
2017-02-05 21:35:28 -0600

    Fixed missing function on playersonline page
    From the frontpage you can click on the number of users online for any
    given realm.  This should take you to the usersonline page and show the
    respectful realm.  The function to check, or set the current realm based
    on the link was missing so this was added.

--- inc/server/server.playersonline.php
+++ inc/server/server.playersonline.php
@@ -13,7 +13,18 @@ if(INCLUDED !== TRUE) {
 	echo "Not Included!"; 
 	exit;
 }
-$pathway_info[] = array('title'=>$lang['online_players'],'link'=>'');
+
+// Check to see if the changerealm_to variable is set in the URI.  If so we need to set the selected 
+// realm cookie and reload the page in order to pull the players online from the correct realm
+if(isset($_GET['changerealm_to']))
+{
+	setcookie("cur_selected_realm", $_GET['changerealm_to'], time() + (3600 * 24 * 365));
+	redirect("?p=server&sub=playersonline",1);
+}
+
+// ==================== //
+$pathway_info[] = array('title' => $lang['online_players'], 'link' => '?p=server&sub=playersonline');
+$pathway_info[] = array('title' => $realm['name'], 'link' => '');
 // ==================== //
 
 // Tell the cache not to cache the file because we want live feeds
